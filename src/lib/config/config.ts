aa07716875cf99e0770614f134a20acb477315b3
0x776F6C66 <132518910+calvin-kimani@users.noreply.github.com>
2024-07-17 21:57:28 +0300

    Add files via upload

--- src/lib/config/config.ts
+++ src/lib/config/config.ts
@@ -1,22 +1,31 @@
 export const user = {
-  first_name: "Giga",
-  last_name: "Chad",
-  tagline: "I'm a teapot", 
+  first_name: "Calvin",
+  last_name: "Kimani",
+  tagline: "Fullstack Developer", 
   about: `
       <p>
-          Hi, I'm THE chad.
+          Hi, I'm Calvin. I'm a full stack developer with a deep passion for computers and cybersecurity particularly binary research. My greatest passions are learning and building new things. I thrive on tackling complex problems and creating innovative solutions.
       </p>
+
+      <p>
+        When I'm not immersed in code, you'll find me enjoying apocalyptic novels filled with mind games and unexpected twists, or engaging in a challenging game of chess. I'm also a hardcore gamer who loves anything that stimulates my mind.
+      </p>
+
+      <p>
+        Whether you're a potential client or just curious about my work, I'm always excited to connect and tackle new challenges.
+      </p>
+
   `,
   avatar: "",
   socials:{
-    email: "giga@chad.com",
-    github: "https://github.com/giga-chad",
-    x: "https://x.com/giga-chad",
-    linkedin: "https://www.linkedin.com/in/giga-chad",
+    email: "calvinkimani475@gmail.com",
+    github: "https://github.com/calvin-kimani",
+    x: "https://x.com/0x776F6C66",
+    linkedin: "https://www.linkedin.com/in/calvin-kimani-5567a3231/",
   },
   techstack: [
     {
-      name: "react", color: "blue"
+      name: "react", color: "red"
     }
   ]
 }
